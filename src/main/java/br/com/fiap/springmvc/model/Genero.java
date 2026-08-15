54102b93d3a437670151ebce1130d8f67aff531c
Gustavo-B-Araujo <gubarrios@icloud.com>
2026-08-14 21:30:01 -0300

    Finalização da aula 2.

--- src/main/java/br/com/fiap/springmvc/model/Genero.java
+++ src/main/java/br/com/fiap/springmvc/model/Genero.java
@@ -1,14 +1,26 @@
 package br.com.fiap.springmvc.model;
 
 public enum Genero {
-    TERROR,
-    ROMANCE,
-    ACAO,
-    SUSPENSE,
-    FICCAO_CIENTIFICA,
-    BIOGRAFIA,
-    FILOSOFIA,
-    RELIGIOSO,
-    HISTORIA,
-    CIENCIAS
+    TERROR("TERROR"),
+    ROMANCE("ROMANCE"),
+    ACAO("ACAO"),
+    SUSPENSE("SUSPENSE"),
+    FICCAO_CIENTIFICA("FICCAO_CIENTIFICA"),
+    BIOGRAFIA("BIOGRAFIA"),
+    FILOSOFIA("FILOSOFIA"),
+    RELIGIOSO("RELIGIOSO"),
+    HISTORIA("HISTORIA"),
+    CIENCIAS("CIENCIAS"),
+    DIDATICO("Didatico");
+
+    private final String descricao;
+
+    Genero(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
 }
